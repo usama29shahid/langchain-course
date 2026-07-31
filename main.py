d5f06c5406b7677697bbd326315cbdf219e3fa50
usama29shahid <46965085+usama29shahid@users.noreply.github.com>
2026-07-31 18:58:02 +0000

    adding groq

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
+
+# from langchain_openai import ChatOpenAI
+# from langchain_ollama import ChatOllama
+
 
 load_dotenv()
 
@@ -33,8 +36,9 @@ def main():
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
     # llm = ChatOllama(temperature=0, model="llama3.2:latest")
+    llm = ChatGroq(temperature=0, model="openai/gpt-oss-20b")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
