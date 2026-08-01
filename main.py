d4671f39c92378183ac91d81123e5fecef2db197
usama29shahid <46965085+usama29shahid@users.noreply.github.com>
2026-08-01 21:39:14 +0000

    pydantic addition

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +12,18 @@ from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )    
 
 PROVIDER = "openrouter"
 
@@ -25,7 +39,7 @@ elif PROVIDER == "groq":
     llm = ChatGroq(temperature=0, model="openai/gpt-oss-20b")
 
 tools = [TavilySearch()]
-agent = create_agent(llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main() -> None:
