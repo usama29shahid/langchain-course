5b7a8aa4b9a3c8f59963299f766fb2069fb6fd46
usama29shahid <46965085+usama29shahid@users.noreply.github.com>
2026-08-01 21:16:56 +0000

    tavily feature

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from dotenv import load_dotenv
+import os
 
-load_dotenv
+load_dotenv()
 
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain.chat_models import ChatOpenAI
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+PROVIDER = "openrouter"
+
+if PROVIDER == "openrouter":
+    llm = ChatOpenAI(
+        temperature=0,
+        model="openai/gpt-oss-20b",
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        base_url="https://openrouter.ai/api/v1",
+    )
+
+elif PROVIDER == "groq":
+    llm = ChatGroq(temperature=0, model="openai/gpt-oss-20b")
+
+tools = [TavilySearch()]
+agent = create_agent(llm, tools=tools)
+
+
+def main() -> None:
+    prompt = "What is the weather in Kolkata today?"
+    result = agent.invoke({"messages": HumanMessage(content=prompt)})
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
